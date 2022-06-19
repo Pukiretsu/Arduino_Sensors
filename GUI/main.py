8eda73829ee3284a851aa6f847860218a8e1f3db
Angel Gonzalez <angonzalezpa@unal.edu.co>
2022-06-19 15:21:34 -0500

    Many Updates
    Updating GitIgnore
    Adding Kivy app (Just a template)

--- GUI/main.py
+++ GUI/main.py
@@ -1,17 +1,23 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
+import plyer as pl
 
 kivyFile = open("GUI\layout.kv","r")
 layout = kivyFile.read()
 kivyFile.close()
+
+Notf = pl.notification
 
 class Main(MDApp):
 
     def build(self):
         self.icon = "GUI/assets/appico.png"
         return Builder.load_string(layout)
+        
 
     def text_Action(self):
         label = self.root.ids.Texto
-        label.text = "Pulsaste el boton."
+        label.text = "Pulsaste el boton." 
+        Notf.notify("Amongus", "A M O N G U S")
+                
 Main().run()
