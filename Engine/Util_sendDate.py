6bce5638df9991b428b6573365bf4949b104dda8
Angel Gonzalez <angonzalezpa@unal.edu.co>
2022-06-21 13:23:03 -0500

    Arduino Data Interface

--- Engine/Util_sendDate.py
+++ Engine/Util_sendDate.py
@@ -2,14 +2,14 @@ import calendar
 import datetime
 import serial
  
-date = datetime.datetime.utcnow()
-utc_time = calendar.timegm(date.utctimetuple())
-BTData = str(utc_time)
-
+date = datetime.datetime.now()
+date = str(date)
+date = date[2:date.index(".")]
+print(date)
 bt_Module = serial.Serial('COM5', 9600)
 
 try:
-    for char in BTData:
+    for char in date:
         bt_Module.write(bytes(char,'utf-8'))
     bt_Module.write(b'\n')
 except:
--- Engine/ino/Sensors/Sensors.ino
+++ Engine/ino/Sensors/Sensors.ino
@@ -6,69 +6,33 @@
 
 
 // Declaracion de Variables Globales
-long ActualUNIXTime;
+String date;
 File dataFile;
 float _TempS1, _TempS2, _TempS3, _TempS4, _TempS5; // Para almacenar la temperatura de los sensores
 // Ubicacion de sensores en cada pin
-int pinS1 = 0; 
-int pinS2 = 1;
-int pinS3 = 2;
-int pinS4 = 3;
-int pinS5 = 4;
 
-void setup() {
-
-  // Configuraciones iniciales
-  // Cambiamos referencia de las entradas analógicas
-  analogReference(INTERNAL);
-  // Configuramos el puerto serial a 9600 bps
-  Serial.begin(9600);
-
-  // Inicializacion de SD (Si no hay el programa no arranca)
-  Serial.println("Iniciando Tarjeta SD...");
-  if (!SD.begin(10)) 
-  { 
-    Serial.println("La tarjeta SD no se pudo inicializar...");
-    while(!SD.begin(10))
-    {
-      delay(1000);
-    }
-  }
-  Serial.println("Conexion exitosa con la Tarjeta SD...");
-  
-  //Obteniendo la fecha y hora desde comunicacion BT
-  getUNIXTime();
-}
-
-void getUNIXTime() 
+String getDataTime() 
 {
+  String BTdata = "";
   while(1)
   {
     Serial.print("Iniciando servicio bluetooth...");
     if(Serial.available()) // Si existe algo en el buffer inicia la operacion
     {
       Serial.println("Ok...");
-      // Variables privadas para funcionamiento
-      long UnixTime;
-      String BTdata = "";
 
       // Inicio de ciclo de recepcion de datos
       while(Serial.available())
       {
-        long RCdataChar = Serial.read();
-        if(isDigit(RCdataChar))
+        char RCdataChar = Serial.read();
+        if(RCdataChar!='\n')
         {
-          BTdata += (char)RCdataChar;
+          BTdata += RCdataChar;
         }
-        if(RCdataChar=='\n')
+        else
         {
-          UnixTime = BTdata.toInt();
-          Serial.print("Tiempo UNIX: ");
-          Serial.println(UnixTime);
           // Limpieza tras obtencion de datos.
           Serial.flush();
-          BTdata = "";
-          
         }
       }
       break;
@@ -79,9 +43,55 @@ void getUNIXTime()
       delay(3000);
     }  
   }
+  return BTdata;
+}
+
+void setup() 
+{
+  // Configuraciones iniciales
+  // Cambiamos referencia de las entradas analógicas
+  analogReference(INTERNAL);
+  // Configuramos el puerto serial a 9600 bps
+  Serial.begin(9600);
+
+  // Inicializacion de SD (Si no hay el programa no arranca)
+  Serial.print("Iniciando Tarjeta SD...");
+  if (!SD.begin(10)) 
+  { 
+    Serial.println("La tarjeta SD no se pudo inicializar...");
+    while(!SD.begin(10))
+    {
+      delay(1000);
+    }
+  }
+  Serial.println("Ok...");
   
-    
-  //return UnixTime;
+  //Obteniendo la fecha y hora desde comunicacion BT
+  date = getDataTime();
+  //Creacion del archivo en la SD
+  Serial.print("Iniciando archivo de datos en la SD...");
+  String filename = date.substring(0,8) + ".csv";
+  if(SD.exists(filename))
+  {
+    Serial.println("Ok...");
+    Serial.println("CSV configurado...");
+  }
+  else
+  {
+    dataFile = SD.open(filename, FILE_WRITE);
+    if (dataFile)
+    {
+      Serial.println("Ok...");
+      Serial.print("Configurando CSV...");
+      dataFile.println("S1,S2,S3,S4,S5,TimeStamp");
+      dataFile.close();
+      Serial.print("Ok...");
+    }
+    else 
+    {
+      Serial.println("Error...");
+    }
+  }
 }
 
 void loop() {
