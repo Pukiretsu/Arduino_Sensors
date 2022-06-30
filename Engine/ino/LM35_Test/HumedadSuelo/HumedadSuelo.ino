a3ea5f1ffc034f20fd16b201939ade87570ee2cb
Angel Gonzalez <angonzalezpa@unal.edu.co>
2022-06-30 15:11:39 -0500

    Humedad

--- Engine/ino/LM35_Test/HumedadSuelo/HumedadSuelo.ino
+++ Engine/ino/LM35_Test/HumedadSuelo/HumedadSuelo.ino
@@ -1,14 +1,14 @@
 //Arduino Code - Humedad del suelo
  
-int PinSeñal = A0; //Definimos el pin para el arduino
+int PinSensor = A0; //Definimos el pin para el arduino
  
 void setup() {
   Serial.begin(9600);
 }
  
 void loop() {
-  int humedad = analogRead(PinSeñal);
+  int humedad = analogRead(PinSensor);
   Serial.print("Nivel de humedad del suelo: ");
-  Serial.println(Humedad);
+  Serial.println(humedad);
   delay(1000);
 }
